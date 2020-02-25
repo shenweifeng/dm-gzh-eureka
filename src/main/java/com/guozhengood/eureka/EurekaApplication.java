af7e978c91a14997b2528f441562c803d20dab9e
shenweifeng <feng2002feng@163.com>
2020-02-25 15:00:25 +0800

    dm-gzh-eureka.iml

---
+++ src/main/java/com/guozhengood/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.guozhengood.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: swf
+ * @Description:
+ * @Date:Create：in 2020/2/19:20:05
+ * @Modified By：
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
